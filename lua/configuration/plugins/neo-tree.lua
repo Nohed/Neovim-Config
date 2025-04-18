acffe703ebc6d582d86a4af17a2137de3e44b040
Noel Hedlund <c22nhd@cs.umu.se>
2025-04-18 19:45:32 +0200

    remade structure

---
+++ lua/configuration/plugins/neo-tree.lua
@@ -0,0 +1,34 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    {
+      '<leader>e',
+      function()
+        require('neo-tree.command').execute {
+          toggle = true,
+          position = 'left',
+        }
+      end,
+      desc = 'Buffers (root dir)',
+    },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
