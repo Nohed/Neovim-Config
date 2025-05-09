aa351c5e2740ddecd425377aca3ddb1c3eb4bd15
Noel Hedlund <c22nhd@cs.umu.se>
2025-05-09 20:40:08 +0200

    Changed dashboard

--- lua/configuration/plugins/snack.lua
+++ lua/configuration/plugins/snack.lua
@@ -11,19 +11,13 @@ return {
         dashboard = {
             enabled = true,
             sections = {
-                {
-                    section = 'terminal',
-                    cmd = 'chafa ~/.config/nvim/dashboard_image.png --format symbols --symbols vhalf --size 60x25 --stretch; sleep .1',
-                    height = 25,
-                    padding = 1,
-                },
+                { section = 'header' },
                 { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
                 { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
                 { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
                 { section = 'startup' },
             },
         },
-
         indent = { enabled = true },
         input = { enabled = true },
         quickfile = { enabled = true },
